e1efbc110a1fc4697ef708fb341827a616f683b3
bahga <43752565+bahga@users.noreply.github.com>
2020-02-18 16:50:57 -0500

    Updated splash page and javascript files

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-ECHO is on.
+/*
+ * Filename: index.js
+ * Author: Glenn Abastillas
+ * Date: February 18, 2020
+ * 
+ * JavaScript functionality for pages under the domain name
+ * www.glennabastillas.com
+ */
+
+ $(document).ready(function()
+ {
+    // Enable scrolling for interactive cv
+    $("#cv").click(function(){
+        var cv_background = $("#p2");
+        var cv_background_top = cv_background.position().top;
+
+        var scrollTo = {top : cv_background_top,
+                        left : 0,
+                        behavior : 'smooth'};
+
+        window.scrollTo(0, cv_background_top);
+    });
+
+ })
